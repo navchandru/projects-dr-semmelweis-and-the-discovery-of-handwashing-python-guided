ba5e68ad3daacd389e6788f9a31d6651a3b3bc28
Rasmus Bååth <rasmus.baath@gmail.com>
2017-10-21 19:31:14 +0200

    Removed ---

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,11 @@
-# If bash command fails, build should error out
-set -e
 
-##### Install specific package versions with pip #####
+pip3 install numpy==1.11.0
+pip3 install pandas==0.19.1
+pip3 install scipy==0.18.1
+pip3 install seaborn==0.7.1
+pip3 install wquantiles==0.5
 
-# pip3 install pandas==0.19.1
-# pip3 install tweepy==3.5.0
-# pip3 install bs4==4.4.1
+#DATADIR=/usr/local/share/datasets
+#mkdir -p $DATADIR
+#wget -O $DATADIR/names.csv.gz https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/names.csv.gz
+#wget -O $DATADIR/lifetables.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/lifetables.csv
