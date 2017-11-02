23dccd37528728020e701c40b70f7e1db0a1a675
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-02 14:46:09 +0100

    Added project image

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,7 @@ pip3 install pandas==0.21.0
 
 DATADIR=/home/repl/datasets
 ASSETURL=https://s3.amazonaws.com/assets.datacamp.com/production/project_20/datasets
-FILES="monthly_deaths.csv yearly_deaths_by_clinic.csv ignaz_semmelweis_1860.jpeg"
+FILES="monthly_deaths.csv yearly_deaths_by_clinic.csv ignaz_semmelweis_1860.jpeg project_image.png"
 
 mkdir -p $DATADIR
 for FILE in $FILES
