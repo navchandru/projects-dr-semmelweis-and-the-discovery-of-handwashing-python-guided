cce051b47644fa342392d3568fd6063f2960770a
Rasmus Bååth <rasmus.baath@gmail.com>
2017-11-02 13:19:27 +0100

    Correctly upload assets

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,15 @@
+# pip3 install pandas==0.19.1
 
-#pip3 install numpy==1.11.0
-#pip3 install pandas==0.19.1
-#pip3 install scipy==0.18.1
+DATADIR=/home/repl/datasets
+ASSETURL=https://s3.amazonaws.com/assets.datacamp.com/production/project_20/datasets
+FILES="monthly_deaths.csv yearly_deaths_by_clinic.csv"
 
-DATADIR=/home/repl
 mkdir -p $DATADIR
-wget -O $DATADIR/monthly_deaths.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_20/datasets/monthly_deaths.csv
-wget -O $DATADIR/yearly_deaths_by_clinic.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_20/datasets/yearly_deaths_by_clinic.csv
+for FILE in $FILES
+do
+	echo wget -O $DATADIR/$FILE $ASSETURL/$FILE
+done
+
+
+#wget -O $DATADIR/monthly_deaths.csv $ASSETURL/monthly_deaths.csv
+#wget -O $DATADIR/yearly_deaths_by_clinic.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_20/datasets/yearly_deaths_by_clinic.csv
